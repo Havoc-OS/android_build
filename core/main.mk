1c5fdf16f00e9a02829f42e2ab67db7ee721d878
cybojenix <anthonydking@gmail.com>
2013-11-02 17:18:36 +0400

    core: remove all the spam about including make files
    Change-Id: Ifd2ef0e239d16b169c572472416c2c31df62a367
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -443,7 +443,7 @@ subdir_makefiles := $(SOONG_ANDROID_MK) $(file <$(OUT_DIR)/.module_paths/Android
 subdir_makefiles_total := $(words int $(subdir_makefiles) post finish)
 .KATI_READONLY := subdir_makefiles_total
 
-$(foreach mk,$(subdir_makefiles),$(info [$(call inc_and_print,subdir_makefiles_inc)/$(subdir_makefiles_total)] including $(mk) ...)$(eval include $(mk)))
+$(foreach mk,$(subdir_makefiles),$([$(call inc_and_print,subdir_makefiles_inc)/$(subdir_makefiles_total)])$(eval include $(mk)))
 
 ifneq (,$(PDK_FUSION_PLATFORM_ZIP)$(PDK_FUSION_PLATFORM_DIR))
 # Bring in the PDK platform.zip modules.
