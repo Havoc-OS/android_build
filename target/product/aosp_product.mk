41e32e4dabbb4c22872126c2a8819d7039164682
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-04-25 17:25:42 -0300

    build: Cleanup aosp_product
    Change-Id: I34165abb9427e88d510e0d304e9dbd12a17a0124
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -18,29 +18,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-# Default AOSP sounds
-ifeq ($(LINEAGE_BUILD),)
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
-
-# Additional settings used in all AOSP builds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg \
-    ro.com.android.dataroaming?=true \
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
     messaging \
     PhotoTable \
     preinstalled-packages-platform-aosp-product.xml \
-    WallpaperPicker \
+    WallpaperPicker
 
-# Telephony:
-#   Provide a APN configuration to GSI product
-ifeq ($(LINEAGE_BUILD),)
-PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-endif
