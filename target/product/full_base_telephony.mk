cc32c629bc9a733a651dd2ea16c0ffc07aa9557d
Han Wang <416810799@qq.com>
2021-06-09 09:49:54 +0200

    Fix PRODUCT_PROPERTY_OVERRIDES usage in full_base_telephony.mk
    Change-Id: Iac20b297ecefb6b3761c422bbe55c11fe9bc481a
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -23,7 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
 
 ifeq ($(HAVOC_BUILD),)
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true
 endif
 
