272d11fdf97f80ed31a6cdb73af9b73224c970a6
Kelvin Zhang <zhangkelvin@google.com>
2021-08-27 15:12:08 -0700

    Check for vintf compatiblity early in generation process
    Currently CheckVintfIfTrebleEnabled is called after generating payload.
    Users have to wait for ~1 hour before vintf is checked. Do the check
    early and fail early.
    
    Test: th
    Change-Id: I2e4a8c7eaa62c94c92973c127b1d825a26e63b35
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1928,6 +1928,8 @@ def GenerateAbOtaPackage(target_file, output_file, source_file=None):
   elif OPTIONS.skip_postinstall:
     target_file = GetTargetFilesZipWithoutPostinstallConfig(target_file)
 
+  CheckVintfIfTrebleEnabled(target_file, target_info)
+
   # Generate payload.
   payload = Payload()
 
@@ -1980,8 +1982,6 @@ def GenerateAbOtaPackage(target_file, output_file, source_file=None):
       logger.warning("Cannot find care map file in target_file package")
 
   common.ZipClose(target_zip)
-
-  CheckVintfIfTrebleEnabled(target_file, target_info)
 
   # We haven't written the metadata entry yet, which will be handled in
   # FinalizeMetadata().
